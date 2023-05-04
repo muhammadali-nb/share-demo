3706804e74b4724488ccc61cff253ed56a0470b9
muhammadali <101570725+yota69@users.noreply.github.com>
2023-05-04 15:36:22 +0500

    ali update

--- src/components/ui/wallet.tsx
+++ src/components/ui/wallet.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Wallet = () => {
 	return (
-		<div className="flex justify-between items-center px-6 py-5 bg-zinc-100 rounded-xl">
+		<div className="flex justify-between items-center p-6 bg-zinc-100 rounded-xl">
 			<div className="flex items-center">
 				<Image src="/icons/wallet.svg" alt="Wallet" width={26} height={28} />
 				<h3 className="text-2xl font-medium ml-2 first-letter:text-blue-400">
