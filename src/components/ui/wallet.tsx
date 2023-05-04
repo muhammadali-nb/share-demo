99e0d7edbb38b420ece8f66868e55862866dc3c5
muhammadali <101570725+yota69@users.noreply.github.com>
2023-05-04 15:22:47 +0500

    ali update

--- src/components/ui/wallet.tsx
+++ src/components/ui/wallet.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 
 const Wallet = () => {
-  return (
-    <div className='flex justify-between items-center px-5 py-6 bg-zinc-100 rounded-xl'>
-      <div className='flex items-center'>
-        <Image src='/icons/wallet.svg' alt='Wallet' width={26} height={28}/>
-        <h3 className='text-xl font-medium ml-2'>Wallet</h3>
-      </div>
-      <p className='text-md font-medium'>13.0231 USD</p>
-    </div>
-  );
+	return (
+		<div className="flex justify-between items-center px-6 py-5 bg-zinc-100 rounded-xl">
+			<div className="flex items-center">
+				<Image src="/icons/wallet.svg" alt="Wallet" width={26} height={28} />
+				<h3 className="text-2xl font-medium ml-2 first-letter:text-blue-400">
+					Wallet
+				</h3>
+			</div>
+			<p className="text-md font-medium">13.0231 USD</p>
+		</div>
+	);
 };
 
 export default Wallet;
